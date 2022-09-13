fdf6c99a25b526f1a1de7fc0f34a700d23dea7c4
m.maksiutov <m.maksiutov@bars.group>
2022-09-13 18:09:22 +0300

    basic

--- src/main/java/bedolagi/strategy/StrategyApplication.java
+++ src/main/java/bedolagi/strategy/StrategyApplication.java
@@ -1,13 +1,22 @@
 package bedolagi.strategy;
 
-import org.springframework.boot.SpringApplication;
+import bedolagi.strategy.impl.GeneralBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class StrategyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StrategyApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		BufferedReader fileReader = new BufferedReader(new FileReader("src/main/resources/trash/words_only.json"));
+		List<String> words = fileReader.lines()
+				.collect(Collectors.toList());
+
+		new GeneralBuilder(words).build();
 	}
 
 }
